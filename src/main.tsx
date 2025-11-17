485a9ff7506e31d8ea908910c0351010d61bc72e
Ansar mehmood <ytaura09@gmail.com>
2025-11-17 20:28:13 +0500

    new changes for removing logos section

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,17 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Unregister any existing service workers to prevent caching issues
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (const registration of registrations) {
+      registration.unregister().then((success) => {
+        if (success) {
+          console.log("Service worker unregistered successfully");
+        }
+      });
+    }
+  });
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -14,7 +14,7 @@ const Index = () => {
     <div className="min-h-screen">
       <Navigation />
       <Hero />
-      <SocialProof />
+      
       <VideoSection />
       <Outcomes />
       <IndustriesSection />
